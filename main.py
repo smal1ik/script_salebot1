46c721e3dda94aaf9303754b9f1d938c8a591cc7
MATVEI <motyha18@gmail.com>
2023-11-29 23:45:09 +0800

    add

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from fastapi.responses import FileResponse
 from image_generator import *
 from pathlib import Path
 import uvicorn
-import os
+
 app = FastAPI()
 
 
 @app.get('/api/get')
 async def get(url_avatar, answers):
-    dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     uuid = generate(url_avatar)
-    image_path = Path(f"{dir}/generated_images/{uuid}.jpg")
+    image_path = Path(f"generated_images/{uuid}.jpg")
 
     return FileResponse(image_path)
 
